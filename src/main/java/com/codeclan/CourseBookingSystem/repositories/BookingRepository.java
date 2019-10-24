432f286e6bf4f1920496d0beb17b0aabcfd21aa3
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-24 14:22:00 +0100

    get all bookings for a given date

--- src/main/java/com/codeclan/CourseBookingSystem/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/CourseBookingSystem/repositories/BookingRepository.java
@@ -4,6 +4,10 @@ import com.codeclan.CourseBookingSystem.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findBookingsByDate(String date);
 }
