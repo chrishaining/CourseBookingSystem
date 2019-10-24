48200d4a68c56a0320f858c23eb1af8311226a09
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-24 11:37:40 +0100

    add tables

--- src/main/java/com/codeclan/CourseBookingSystem/models/Customer.java
+++ src/main/java/com/codeclan/CourseBookingSystem/models/Customer.java
@@ -1,13 +1,22 @@
 package com.codeclan.CourseBookingSystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "town")
     private String town;
 
+    @Column(name = "age")
     private int age;
 
     public Customer(String name, String town, int age) {
